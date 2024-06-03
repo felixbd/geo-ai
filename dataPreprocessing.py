d96ab83b91bf02620521ea0259a476eebd11e415
Felix D <fdrees@uni-bremen.de>
2024-06-03 20:23:16 +0200

    LAST time, Maciej, /dev/urandom IS NOT a variable name generator...

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -9,9 +9,14 @@ geo ai test
 import polars as pl
 
 
+TEST_IMAGE_PATH: str = "./images/6zM_LlnQo9JD9zALzWdKtw.jpeg"
+
 
 def main() -> None:
-	pass
+    df = pl.read_csv("./images.csv")
+    print(df.head(5))
+
+    return
 
 
 if __name__ == "__main__":
---
+++ map.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+world map with grid every 0.5 degrees (latitude, longitude)
+"""
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+
+ROBINSON: bool = True
+
+
+def main() -> None:
+    # Create a figure and an axes with a specific projection
+    fig = plt.figure(figsize=(15, 10))
+    if not ROBINSON:
+        ax = plt.axes(projection=ccrs.PlateCarree())
+    else:
+        ax = plt.axes(projection=ccrs.Robinson())
+
+
+    # Add features to the map (e.g., coastlines, borders)
+    ax.add_feature(cfeature.COASTLINE)
+    ax.add_feature(cfeature.BORDERS)
+
+    # Draw gridlines every 0.5 degrees
+    gl = ax.gridlines(draw_labels=True, linestyle='--')
+    gl.xlocator = plt.FixedLocator(range(-180, 181, 1))
+    gl.ylocator = plt.FixedLocator(range(-90, 91, 1))
+
+    # Add gridlines every 0.5 degrees
+    for lat in range(-90, 91):
+        ax.plot([-180, 180], [lat, lat], color='gray', linestyle='--', linewidth=0.5, transform=ccrs.PlateCarree())
+    for lon in range(-180, 181):
+    	ax.plot([lon, lon], [-90, 90], color='gray', linestyle='--', linewidth=0.5, transform=ccrs.PlateCarree())
+
+    # Set the extent of the map to the world
+    # ax.set_extent([-180, 180, -90, 90], crs=ccrs.PlateCarree())
+
+    # Show the plot
+    plt.show()
+
+
+if __name__ == "__main__":
+	main()
