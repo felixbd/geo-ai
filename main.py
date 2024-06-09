efb09d8843376e3697024c9aede005821fe71c0e
Felix D <fdrees@uni-bremen.de>
2024-06-09 15:41:38 +0200

    Check next commit for message.

--- main.py
+++ main.py
@@ -4,14 +4,13 @@
 geo ai
 """
 
+
 import polars as pl
 from tqdm import tqdm
 
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import numpy as np
 
 X_SCALE: int = 10  # 10  # 1000
 Y_SCALE: int = 10  # 20 # 2000
@@ -67,27 +66,6 @@ def clean_data_frame() -> None:
 # ----------
 
 
-"""
-# Train the model
-history = model.fit(X_train, {'output_1': Y_train[:, 0], 'output_2': Y_train[:, 1]}, 
-                    epochs=50, 
-                    batch_size=32, 
-                    validation_split=0.2)
-
-# Make predictions
-Y_pred = model.predict(X_test)
-y_pred_1 = Y_pred[0]
-y_pred_2 = Y_pred[1]
-
-# Evaluate the model
-accuracy_1 = np.mean((y_pred_1 > 0.5).astype(int) == Y_test[:, 0])
-accuracy_2 = np.mean((y_pred_2 > 0.5).astype(int) == Y_test[:, 1])
-
-print(f"Accuracy for the first label set: {accuracy_1}")
-print(f"Accuracy for the second label set: {accuracy_2}")
-"""
-
-
 def generate_model() -> tf.keras.models.Model:
     # maybe use gelu activation function instead of relu
 
@@ -123,6 +101,9 @@ def generate_model() -> tf.keras.models.Model:
 
 
 def train_model(model: tf.keras.models.Model) -> None:
+    rv: list = []
+
+
     df = pl.read_csv("./new-images.csv")
 
     temp = 5  # df.shape[0]
@@ -132,21 +113,24 @@ def train_model(model: tf.keras.models.Model) -> None:
         lat, lon = row[1], row[2]
         image_path = f"./images/{row[0]}.jpeg"
 
+        rv.append(f"[{i}] -> {lat}:{lon} :: {image_path}")
+
         # Load the image
         image = tf.io.read_file(image_path)
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, (target_height, target_width))
         image = image / 255.0
 
-        # Load the labels
-        label_1 = lat
-        label_2 = lon
-
         # Train the model
-        model.fit(image, {'output_1': label_1, 'output_2': label_2}, epochs=50, batch_size=32)  # , validation_split=0.2)
+        # FIXME: model.fit(image, {'output_1': lat, 'output_2': lon})  # , epochs=50, batch_size=32)  # , validation_split=0.2)
 
     # save the model
-    model.save("model.h5")
+    # model.save("model.h5")
+
+    for e in rv:
+        print(e)
+
+    return
 
 
 def main() -> None:
